f04136601257a23d5dcfdea4d7c91fb79042d687
Tim A. Conner <timaconner1@gmail.com>
2018-01-26 09:07:14 -0600

    Add Poisonous & Non-poisonous filter.

--- js/controllers/MushroomCtrl.js
+++ js/controllers/MushroomCtrl.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function($scope, MushroomFactory){
-    console.log('mushroomCtrl');
     $scope.pois = true;
 
     MushroomFactory.getMushrooms().then(mushroomData => {
